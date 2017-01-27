4e4bc7db63a30aac0ee6a1451509e4f7c585eab9
Arrian Pascual <arrian.pascual@SAT-A.COM.PH>
2017-01-27 14:56:49 +0800

    MOD: Navigation Bar

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: `./app/app.html`,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+	private 		mIsCollapse: 	boolean;
+	private			mRenderer:		Renderer;
+
+	@ViewChild("menuToggle")
+	private 	_menuToggle: ElementRef;
+
+	constructor (_renderer: Renderer)
+	{
+		/// Initialize Member Variables
+		this.mIsCollapse = true;
+		this.mRenderer = _renderer;
+	}
+
+	private OnClickToggleBtn () {
+		this.mIsCollapse = !this.mIsCollapse;
+		if (this.mIsCollapse == true) {
+			this.mRenderer.setElementClass (this._menuToggle.nativeElement, "topbar-responsive", false);
+		}
+		else {
+			this.mRenderer.setElementClass (this._menuToggle.nativeElement, "topbar-responsive", true);
+		}
+	}
+}
+
